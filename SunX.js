1defaf24b1d8a46818275cff8b7e23ec75309d42
SunMoon <51331913@qq.com>
2016-09-30 18:02:33 +0800

    添加新功能
    重写广播事件类
    增加双向链表
    修改了获取单例的方式

--- SunX.js
+++ SunX.js
@@ -1,42 +1,258 @@
 var SunX = {};
 
-SunX.SEventManager = cc._Class.extend({
-    _allHandler: null,
+SunX.SLinkListItem = function (Data) {
+    this.pre = null;
+    this.next = null;
+    this.data = Data;
+};
+
+SunX.LinkList = cc._Class.extend({
 
     ctor: function () {
-        this._allHandler = {};
+        this._size = 0;
+        this._head = null;
+        this._end = null;
     },
 
-    AddEvent: function (tag, handler) {
-        cc.assert(cc.js.isNumber(tag), "_resItems no item");
-        let arr = this._allHandler[tag];
-        if (arr === undefined) {
-            this._allHandler[tag] = [handler];
+    Size: function () {
+        return this._size;
+    },
+
+    PushBack: function (data) {
+        if (0 == this._size) {
+            this._head = data;
+            this._end = data;
         } else {
-            arr.push(handler);
+            this._end.next = data;
+            data.pre = this._end;
+            this._end = data;
         }
+        ++ this._size;
     },
 
-    RemoveEvent: function (tag, handler) {
-        let arr = this._allHandler[tag];
-        cc.assert(arr != undefined, "arr is null");
-        cc.js.array.remove(arr, handler);
-    },
-    
-    ExecuteEvent: function (tag, data) {
-        let arr = this._allHandler[tag];
-        cc.assert(arr != undefined, "arr is null");
-
-        let size = arr.length;
-        for (let i = 0; i < size; ++ i) {
-            arr[i](data);
+    PushFront: function (data) {
+        if (0 == this._size) {
+            this._head = data;
+            this._end = data;
+        } else {
+            this._head.pre = data;
+            data.next = this._head;
+            this._head = data;
         }
+        ++ this._size;
+    },
+
+    Remove: function (data) {
+        if (0 == this._size) return;
+        data.pre.next = data.next;
+        -- this._size;
+    },
+
+    Clear: function () {
+
+    },
+
+    GetList: function () {
+        return this._head;
     }
 });
 
-SunX.SEventManager.Singleton = null;
+SunX.SEventEntry = cc._Class.extend({
+
+    ctor: function () {
+        this.tag = 0;
+        this.target = null;
+        this.func = null;
+        this.isDirty = false;
+    },
+
+    InitData: function (Tag, Func, Target) {
+        this.tag = Tag;
+        this.target = Target;
+        this.func = Func;
+    },
+    
+    Execute: function (event) {
+        this.func.call(this.target, event);
+    }
+});
+
+SunX.SEventManager = cc._Class.extend({
+    _hashForEntry: null,
+    _hashListener: null,
+    _removeArray: null,
+    _isRemoveDirty: false,
+
+    ctor: function () {
+        this._hashForEntry = {};
+        this._hashListener = {};
+        this._removeArray = [];
+        this._isRemoveDirty = false;
+
+        cc.director.getScheduler().schedule(this._update, this, 0, false);
+    },
+
+    ExecuteEvent: function (Tag, Data) {
+        var arr = this._hashListener[Tag];
+        var len = arr.length;
+        var entry = null;
+        for (var i = 0; i < len; ++ i) {
+            entry = arr[i];
+            if (entry.isDirty) continue;
+            entry.Execute({tag:Tag, data:Data});
+        }
+    },
+
+    AddEvent: function (tag, handler, target) {
+        cc.assert(cc.js.isNumber(tag), "tag no Number");
+
+        let instanceId = this._getTargetId(target);
+        let element = this._hashForEntry[instanceId];
+
+        if(!element) {
+            element = [];
+            this._hashForEntry[instanceId] = element;
+        }
+
+        let entry = this._findTagToEntry(element, tag);
+
+        if (entry) {
+            if (entry.isDirty) {
+                entry.isDirty = false;
+                cc.js.array.remove(this._removeArray, entry);
+            }
+            return;
+        }
+
+        entry = this._addListenerMap(tag, handler, target);
+        element.push(entry);
+    },
+
+    RemoveEvent: function (tag, target) {
+        let instanceId = this._getTargetId(target);
+        let element = this._hashForEntry[instanceId];
+
+        if (!element) return;
+
+
+        let entry = this._findTagToEntry(entryArr, tag);
+        if (!entry || !entry.isDirty) return;
+
+
+        entry.isDirty = true;
+        this._removeArray.push(entry);
+        this._isRemoveDirty = true;
+    },
+
+    RemoveAllEvent: function (target) {
+        let instanceId = this._getTargetId(target);
+        let element = this._hashForEntry[instanceId];
+
+        if (!element) return;
+
+        let len = element.length;
+        if (0 == len) return;
+
+        let entry = null;
+        for (let i = 0; i < len; ++ i) {
+            entry = element[i];
+            if (entry.isDirty) continue;
+            entry.isDirty = true;
+            this._removeArray.push(entry);
+        }
+
+        this._isRemoveDirty = true;
+    },
+
+    _update: function (dlt) {
+        if (this._isRemoveDirty) {
+            let instanceId = 0;
+            let element = null;
+            let entry = null;
+            let arr = null;
+            let size = 0;
+
+            let len = this._removeArray.length;
+
+            while (--len >= 0) {
+                entry = this._removeArray[len];
+                arr = this._hashListener[entry.tag];
+
+                if (arr) {
+                    instanceId = this._getTargetId(entry.target);
+                    element = this._hashForEntry[instanceId];
+                    if(!element) continue;
+
+                    size = element.length;
+                    this._arrRemoveItem(element, entry, size);
+                    if (1 == size) delete this._hashForEntry[instanceId];
+
+
+                    size = element.length;
+                    this._arrRemoveItem(arr, entry, size);
+                    if (1 == size) delete this._hashListener[entry.tag];
+
+                }
+            }
+
+            this._removeArray.length = 0;
+            this._isRemoveDirty = false;
+        }
+    },
+
+    _addListenerMap: function (tag, handler, target) {
+        let entryArr = this._hashListener[tag];
+
+        if (!entryArr) {
+            entryArr = [];
+            this._hashListener[tag] = entryArr;
+        }
+
+        let entry = new SunX.SEventEntry();
+        entry.InitData(tag, handler, target);
+        entryArr.push(entry);
+        return entry;
+    },
+
+    _findTargetToEntry: function (arr, target) {
+        let len = arr.length;
+        for (let i = 0; i < len; ++ i) {
+            if (target == arr[i].target) return arr[i];
+        }
+        return null;
+    },
+
+    _findTagToEntry: function (arr, tag) {
+        let len = arr.length;
+        for (let i = 0; i < len; ++ i) {
+            if (tag == arr[i].tag) return arr[i];
+        }
+        return null;
+    },
+
+    _arrRemoveItem: function (arr, item, len) {
+        if (1 == len) {
+            arr.pop();
+        } else {
+            let index = arr.indexOf(item);
+            if (index == -1) return;
+
+            if (len - 1 == index) {
+                arr.pop();
+            } else {
+                arr[index] = arr.pop();
+            }
+        }
+    },
+
+    _getTargetId: function (target) {
+        return target.__instanceId || target.uuid;
+    },
+});
+
+SunX.sEventManager = null;
 SunX.SEventManager.InitSingleton = function () {
-    SunX.SEventManager.Singleton = new SunX.SEventManager();
+    SunX.sEventManager = new SunX.SEventManager();
 };
 
 SunX.SResItem = cc._Class.extend({
@@ -185,9 +401,9 @@ SunX.SResManager = cc._Class.extend({
     }
 });
 
-SunX.SResManager.Singleton = null;
+SunX.sResManager = null;
 SunX.SResManager.InitSingleton = function () {
-    SunX.SResManager.Singleton = new SunX.SResManager();
+    SunX.sResManager = new SunX.SResManager();
 };
 
 module.exports = SunX;
