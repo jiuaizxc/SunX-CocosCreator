9c9a7304cd208ae420bfa96c1bf5f060e795c234
SunMoon <51331913@qq.com>
2017-01-22 17:05:22 +0800

    CocosCreator1.4版
    从新设计了资源管理的方法！(1.4）

--- SunX.js
+++ SunX.js
@@ -4,7 +4,7 @@ SunX.SEvent = {
     RES_LOAD_OVER: 9000000000
 };
 
-SunX._SEventEntry = cc._Class.extend({
+var _SEventEntry = cc._Class.extend({
     ctor: function () {
         this.tag = 0;
         this.target = null;
@@ -24,7 +24,7 @@ SunX._SEventEntry = cc._Class.extend({
 });
 
 SunX.sEventManager = null;
-SunX._SEventManager = cc._Class.extend({
+var _SEventManager = cc._Class.extend({
     _hashForEntry: null,
     _hashListener: null,
     _removeArray: null,
@@ -39,17 +39,36 @@ SunX._SEventManager = cc._Class.extend({
         cc.director.getScheduler().schedule(this._update, this, 0, cc.macro.REPEAT_FOREVER, 0, false);
     },
 
+    /**
+     * 发送事件。
+     * @method ExecuteEvent
+     * @param {Number} Tag - 事件名称
+     * @param {Object} Data - 用户数据
+     **/
     ExecuteEvent: function (Tag, Data) {
         var arr = this._hashListener[Tag];
-        var len = arr.length;
-        var entry = null;
-        for (var i = 0; i < len; ++ i) {
-            entry = arr[i];
-            if (entry.isDirty) continue;
-            entry.Execute({tag:Tag, data:Data});
+        if (arr) {
+            var len = arr.length;
+            var entry = null;
+            for (var i = 0; i < len; ++ i) {
+                entry = arr[i];
+                if (entry.isDirty) continue;
+                entry.Execute({tag:Tag, data:Data});
+            }
         }
     },
 
+    /**
+     * 注册事件。
+     *
+     * target对象一定要是继承自cc.Class/cc._Class类才能够注册
+     * 同一个target对象上不能注册事件名称相同的事件
+     *
+     * @method AddEvent
+     * @param {Number} tag - 事件名称(注意tag>=9000000000的名称被SunX引擎暂用)
+     * @param {Function} handler - 事件回调函数
+     * @param {Object} target - 事件调用的对象
+     **/
     AddEvent: function (tag, handler, target) {
         cc.assert(cc.js.isNumber(tag), "tag no Number");
 
@@ -75,6 +94,15 @@ SunX._SEventManager = cc._Class.extend({
         element.push(entry);
     },
 
+    /**
+     * 移除单个事件。
+     *
+     * target对象一定要是继承自cc.Class/cc._Class类才能够注册
+     *
+     * @method RemoveEvent
+     * @param {Number} tag - 事件名称
+     * @param {Object} target - 事件调用的对象
+     **/
     RemoveEvent: function (tag, target) {
         let instanceId = this._getTargetId(target);
         let element = this._hashForEntry[instanceId];
@@ -91,6 +119,14 @@ SunX._SEventManager = cc._Class.extend({
         this._isRemoveDirty = true;
     },
 
+    /**
+     * 移除所有事件。
+     *
+     * target对象一定要是继承自cc.Class/cc._Class类才能够注册
+     *
+     * @method RemoveAllEvent
+     * @param {Object} target - 事件调用的对象
+     **/
     RemoveAllEvent: function (target) {
         let instanceId = this._getTargetId(target);
         let element = this._hashForEntry[instanceId];
@@ -155,7 +191,7 @@ SunX._SEventManager = cc._Class.extend({
             this._hashListener[tag] = entryArr;
         }
 
-        let entry = new SunX._SEventEntry();
+        let entry = new _SEventEntry();
         entry.InitData(tag, handler, target);
         entryArr.push(entry);
         return entry;
@@ -197,13 +233,14 @@ SunX._SEventManager = cc._Class.extend({
     },
 });
 
-SunX._SResItem = cc._Class.extend({
+var _SResItem = cc._Class.extend({
     _uuid: null,
     _loadUrl: null,
     _fullPath: null,
     _type: null,
     _spriteFrameName: null,
     _sceneData: null,
+    _callFunc: null,
 
     ctor: function (uuid, loadUrl, fullPath, type) {
         this._uuid = uuid;
@@ -211,9 +248,9 @@ SunX._SResItem = cc._Class.extend({
         this._fullPath = fullPath;
         this._type = type;
 
-
         this._spriteFrameName = "";
         this._sceneData = [];
+        this._callFunc = null;
     },
 
     GetUUID: function () {
@@ -230,17 +267,25 @@ SunX._SResItem = cc._Class.extend({
 
     GetType: function () {
         return this._type;
+    },
+    
+    GetResID: function () {
+        return this._loadUrl;
     }
 });
 
 SunX.sRes = null;
-SunX._SResManager = cc._Class.extend({
+var _SResManager = cc._Class.extend({
     _pathItemHash: null,
     _uuidItemHash: null,
     _spriteFrameCache: null,
 
     _totalNum: -1,
     _count: -1,
+
+    _cacheRes: null,
+    _cacheSceneRes: null,
+    _curScene: null,
 
     ctor: function () {
         this._pathItemHash = {};
@@ -249,10 +294,15 @@ SunX._SResManager = cc._Class.extend({
 
         this._totalNum = -1;
         this._count = -1;
+
+        this._cacheRes = {};
+        this._cacheSceneRes = {};
+        this._curScene = null;
     },
 
     _initRes: function () {
-        var settings = window._CCSettings;
+        var settings = undefined;
+        if (window) settings = window._CCSettings;
         if (!settings) settings = _CCSettings;
 
         var rawAssets = settings.rawAssets;
@@ -288,27 +338,43 @@ SunX._SResManager = cc._Class.extend({
                     loadUrl = cc.url.raw(fullPath);
                 }
 
-                item = new SunX._SResItem(uuid, loadUrl, fullPath, type);
+                item = new _SResItem(uuid, loadUrl, fullPath, type);
 
                 this._pathItemHash[url] = item;
                 this._uuidItemHash[uuid] = item;
             }
         }
 
-        window._CCSettings = undefined;
+        if(window) window._CCSettings = undefined;
         _CCSettings = undefined;
     },
 
+    /**
+     * 获取精灵帧。
+     * @method GetSpriteFrame
+     * @param {String} name - 精灵帧文件名称
+     **/
     GetSpriteFrame: function (name) {
         return this._spriteFrameCache[name];
     },
 
+    /**
+     * 获取资源。
+     * @method GetRes
+     * @param {String} path - 文件路径
+     **/
     GetRes: function (path) {
         var item = this._pathItemHash[path];
         if (!item) return null;
-        return cc.loader.getRes(item.GetUUID());
+        var url = cc.loader._getReferenceKey(item.GetLoadUrl());
+        return cc.loader.getRes(url);
     },
 
+    /**
+     * 释放资源。
+     * @method ReleaseRes
+     * @param {String|Array} resources - 文件路径/者文件路径的数组
+     **/
     ReleaseRes: function (resources) {
         if (!(resources instanceof Array)) {
             resources = resources ? [resources] : [];
@@ -321,23 +387,80 @@ SunX._SResManager = cc._Class.extend({
             item = this._pathItemHash[path];
             this._releaseOne(item);
         }
+
+        this.PurgeCachedRes();
     },
-    
+
+    /**
+     * 释放掉没有勾选自动释放选项场景的资源
+     *
+     * @method ReleaseSceneRes
+     * @param {String} sceneName - 场景的名称(如果为空,清楚所有已切换过的场景的资源)
+     *
+     **/
+    ReleaseSceneRes: function (sceneName) {
+        if (sceneName == undefined) {
+            for (var key in this._cacheSceneRes) {
+                this._subCacheResArray(this._cacheSceneRes[key]);
+                delete this._cacheSceneRes[key];
+            }
+        } else {
+            var list = this._cacheSceneRes[sceneName];
+            delete this._cacheSceneRes[sceneName];
+            this._subCacheResArray(list);
+        }
+
+        this.PurgeCachedRes();
+    },
+
+    /**
+     *
+     * 清空缓存资源
+     *
+     * @method PurgeCachedRes
+     *
+     **/
+    PurgeCachedRes: function () {
+        for (var key in this._cacheRes) {
+            if (this._cacheRes[key] == 0) {
+                cc.loader.release(key);
+                delete this._cacheRes[key];
+            }
+        }
+    },
+
+    /**
+     * 加载资源。
+     *
+     * @method Load
+     * @param {String|Object|Array} resources - 文件路径/者文件路径的数组
+     **/
     Load: function (resources) {
         if (!(resources instanceof Array)) {
             resources = resources ? [resources] : [];
         }
 
+        var len = resources.length;
+        if (0 == len) return;
+
         var path;
         var item;
-        for (var i = 0; i < resources.length; ++i) {
+        for (var i = 0; i < len; ++i) {
             path = resources[i];
-            item = this._pathItemHash[path];
+
+
+            if (typeof path === 'string') {
+                item = this._pathItemHash[path];
+            } else {
+                item = this._pathItemHash[path.url];
+                item._callFunc = path.func;
+            }
+
             if (!item) {
                 resources[i] = undefined;
             } else {
                 if (cc.isChildClassOf(item._type, cc.Asset)) {
-                    resources[i] = {id: item._loadUrl, type: 'uuid', uuid: item._loadUrl};
+                    resources[i] = {type: 'uuid', uuid: item._loadUrl};
                 } else {
                     resources[i] = item._loadUrl;
                 }
@@ -353,36 +476,67 @@ SunX._SResManager = cc._Class.extend({
         var self = this;
         cc.loader.load(resources, function (error, loadItem) {
             let map = loadItem.map;
+            let data;
             let item;
-            for (var uuid in map) {
-                item = loadItem.getContent(uuid);
-                if (item) {
+            for (var key in map) {
+                item = map[key];
+                data = loadItem.getContent(key);
+                if (data) {
                     // should not release these assets, even if they are static referenced in the scene.
-                    cc.loader.setAutoReleaseRecursively(uuid, false);
-                    self._loadOver(error, item);
+                    cc.loader.setAutoReleaseRecursively(key, false);
+                    self._loadOver(error, key, item.url, data, item.dependKeys);
                 }
             }
         });
     },
 
-    _loadOver: function (error, asset) {
-        var item = null;
-        if (asset.hasOwnProperty("_uuid")) {
-            item = this._uuidItemHash[asset._uuid];
-        }
+    _loadOver: function (error, key, url, asset, dependKeys) {
+        var item = this._uuidItemHash[key];
+        if (!item) item = this._uuidItemHash[asset._uuid];
 
-        if (item) {
-            if (item.GetType() == cc.SpriteFrame) {
-                this._spriteFrameCache[asset.name] = asset;
-                item._spriteFrameName = asset.name;
-            } else if (item.GetType() == cc.SpriteAtlas) {
-                this._loadSpriteAtlas(asset);
-            } else if (item.GetType() == cc.SceneAsset) {
-                var arr = cc.loader.getDependsRecursively(asset);
-                Array.prototype.push.apply(item._sceneData, arr);
+        if (!item) {
+            var index = key.indexOf("resources");
+            if (index >= 0) {
+                var path = key.slice(index + 10);
+                item = this._pathItemHash[path];
             }
         }
 
+        if (item) {
+            var depends;
+            if (item.GetType() == cc.SpriteFrame) {
+                this._spriteFrameCache[asset.name] = asset;
+                item._spriteFrameName = asset.name;
+
+                this._addCacheRes(url);
+                this._addCacheResArray(dependKeys);
+            } else if (item.GetType() == cc.SpriteAtlas) {
+                depends = cc.loader.getDependsRecursively(asset);
+                this._addCacheResArray(depends);
+                this._loadSpriteAtlas(asset);
+            } else if (item.GetType() == cc.SceneAsset) {
+                depends = cc.loader.getDependsRecursively(asset);
+                Array.prototype.push.apply(item._sceneData, depends);
+                if (this._cacheSceneRes[asset.name]) delete this._cacheSceneRes[asset.name];
+                this._addCacheResArray(depends);
+            } else if (item.GetType() == cc.AudioClip) {
+                this._totalNum += 1;
+                this._addCacheRes(url);
+                cc.audioEngine.preload(url, function (b) {
+                    SunX.sRes._loadCount();
+                });
+            } else {
+                depends = cc.loader.getDependsRecursively(key);
+                this._addCacheResArray(depends);
+            }
+
+            if (item._callFunc) item._callFunc(asset);
+        }
+
+        this._loadCount();
+    },
+
+    _loadCount: function () {
         this._count += 1;
         if (this._totalNum == this._count) {
             this._count = -1;
@@ -402,25 +556,30 @@ SunX._SResManager = cc._Class.extend({
     _releaseOne: function (item) {
         if (!item) return;
 
+        var url;
+        var depends;
+        var asset;
+
         if (item.GetType() == cc.SpriteFrame) {
-            delete this._spriteFrameCache[item._spriteFrameName];
-            let temp = cc.loader.getDependsRecursively(item.GetLoadUrl());
-            cc.loader.release(temp);
+            delete this._spriteFrameCache[item._spriteFrameName];//删除时机,有待考虑
+            depends = cc.loader.getDependsRecursively(item.GetLoadUrl());
+            this._subCacheResArray(depends);
         } else if (item.GetType() == cc.SpriteAtlas) {
-            let atlas = cc.loader.getRes(item.GetLoadUrl());
-            if (atlas) this._releaseSpriteAtlas(atlas);
-        } else if (item.GetType() == cc.BitmapFont) {
-            let asset = cc.loader.getRes(item.GetLoadUrl());
-            if (asset) this._releaseBitmapFont(asset);
-        } else if (item.GetType() == cc.Prefab) {
-            let asset = cc.loader.getRes(item.GetLoadUrl());
-            if (asset) this._releasePrefab(asset);
+            url = cc.loader._getReferenceKey(item.GetLoadUrl());
+            asset = cc.loader.getRes(url);
+            if (asset) this._releaseSpriteAtlas(asset);
         } else if (item.GetType() == cc.SceneAsset) {
             this._releaseScene(item);
+        } else if (item.GetType() == cc.AudioClip) {
+            this._subCacheRes(item.GetLoadUrl());
+            cc.audioEngine.uncache(item.GetLoadUrl());
         } else {
-            cc.loader.release(item.GetLoadUrl());
-            if (item._uuid != item._loadUrl) {
-                cc.loader.release(item._uuid);
+            if (item.GetType() == cc.RawAsset) {
+                this._subCacheRes(item.GetLoadUrl());
+            } else {
+                url = cc.loader._getReferenceKey(item.GetLoadUrl());
+                asset = cc.loader.getRes(url);
+                if (asset) this._releaseDepends(asset);
             }
         }
     },
@@ -430,79 +589,92 @@ SunX._SResManager = cc._Class.extend({
         for (var key in spriteFrames) {
             delete this._spriteFrameCache[key];
         }
-        var temp = cc.loader.getDependsRecursively(atlas);
-        cc.loader.release(temp);
+
+        this._releaseDepends(atlas);
     },
 
-    _releaseBitmapFont: function (asset) {
-        var arr = asset.rawUrls;
-        var len = arr.length;
-        for (var i = 0; i < len; ++ i) {
-            cc.loader.release(arr[i]);
-        }
-
+    _releaseDepends: function (asset) {
         var temp = cc.loader.getDependsRecursively(asset);
-        cc.loader.release(temp);
-    },
-
-    _releasePrefab: function (asset) {
-        var uuid;
-        var arr = cc.loader.getDependsRecursively(asset);
-        var len = arr.length;
-        for (var i = 0; i < len; ++ i) {
-            uuid = arr[i];
-            if (uuid == asset._uuid) continue;
-            this._releaseOne(this._uuidItemHash[uuid]);
-        }
-        cc.loader.release(asset);
+        this._subCacheResArray(temp);
     },
 
     _releaseScene: function (item) {
-        var arr = item._sceneData;
-        var len = arr.length;
-        for (var i = 0; i < len; ++ i) {
-            uuid = arr[i];
-            if (uuid == item._uuid) continue;
-            this._releaseOne(this._uuidItemHash[uuid]);
-        }
-        cc.loader.release(item._uuid);
+        this._subCacheResArray(item._sceneData);
         item._sceneData.length = 0;
+    },
+    
+    _onSceneLaunch: function (scene) {
+        var curSave = true;
+
+        var sceneInfo = cc.director._getSceneUuid(scene.name);
+
+        if (this._cacheRes[sceneInfo.uuid]) curSave = false;
+        this._curScene = scene;
+
+        if (curSave) {
+            if (!this._curScene.autoReleaseAssets) {
+                if (!this._cacheSceneRes[this._curScene.name]) {
+                    this._addCacheSceneRes(this._curScene);
+                    this._addCacheResArray(this._curScene.dependAssets);
+                }
+            }
+        }
+    },
+
+    _addCacheResArray: function (list) {
+        var len = list.length;
+        for (var i = 0; i < len; ++ i) {
+            this._addCacheRes(list[i]);
+        }
+    },
+    
+    _addCacheRes: function (url) {
+        var refCount = this._cacheRes[url];
+
+        if (refCount == undefined) {
+            this._cacheRes[url] = 1;
+        } else {
+            ++ refCount;
+            this._cacheRes[url] = refCount;
+        }
+    },
+    
+    _addCacheSceneRes: function (scene) {
+        var array = [];
+        Array.prototype.push.apply(array, scene.dependAssets);
+        this._cacheSceneRes[scene.name] = array;
+    },
+
+    _subCacheResArray: function (list) {
+        var len = list.length;
+        for (var i = 0; i < len; ++ i) {
+            this._subCacheRes(list[i]);
+        }
+    },
+
+    _subCacheRes: function (url) {
+        var refCount = this._cacheRes[url];
+
+        if (refCount == undefined) return;
+
+        if (refCount > 0) {
+            -- refCount;
+            this._cacheRes[url] = refCount;
+        }
     }
 });
 
 SunX.sUserData = null;
-SunX._SUserData = cc._Class.extend({
+var _SUserData = cc._Class.extend({
     _saveFunc: null,
     _readFunc: null,
     
     ctor: function () {
-        /*if (cc.sys.isNative)
-        {
-            this._saveFunc = this._nativeSave;
-            this._readFunc = this._nativeRead;
-        }
-        else
-        {
-            this._saveFunc = this._webSave;
-            this._readFunc = this._webRead;
-        }*/
-
         this._saveFunc = this._webSave;
         this._readFunc = this._webRead;
     },
 
     FindFile: function (fileName) {
-        /*if (cc.sys.isNative)
-        {
-            fileName = jsb.fileUtils.getWritablePath() + fileName + ".sm";
-            return jsb.fileUtils.isFileExist(fileName);
-        }
-        else
-        {
-            if (cc.sys.localStorage.getItem(fileName)) return true;
-            else return false;
-        }*/
-
         if (cc.sys.localStorage.getItem(fileName)) return true;
         else return false;
     },
@@ -532,20 +704,14 @@ SunX._SUserData = cc._Class.extend({
     }
 });
 
-SunX.SInit = function () {
-    SunX.sEventManager = new SunX._SEventManager();
-    SunX.sRes = new SunX._SResManager();
-    SunX.sUserData = new SunX._SUserData();
+cc.game.on(cc.game.EVENT_GAME_INITED, function () {
+    SunX.sEventManager = new _SEventManager();
+    SunX.sRes = new _SResManager();
+    SunX.sUserData = new _SUserData();
+
+    cc.director.on(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function(event) {
+        SunX.sRes._onSceneLaunch(event.detail);
+    });
 
     SunX.sRes._initRes();
-};
-
-SunX.SChangeScene = function (sceneName) {
-    cc.director.loadScene(sceneName, function (error, scene) {
-        var item = SunX.sRes._uuidItemHash[scene.uuid];
-        if (item._sceneData.length > 0) return;
-        Array.prototype.push.apply(item._sceneData, scene.dependAssets);
-    });
-};
-
-//module.exports = SunX;
+});
